784abede3fb7052a1b0d86d7790af7fec950aa49
Sulaiman <sulaimanmaarij@gmail.com>
2019-12-30 18:22:14 +0000

    Scene Management has been complete, the player can transition between scenes using the portal that will persist and update the player's location. Fader has also been implemented, the screen will fade in and out between scenes. This can later on be changed to a loading screen image

---
+++ RPG/Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        public GameObject persistentObjectPrefab;
+
+        static bool hasSpawned = false; //static variable lives throughout the whole application, rather than dying with classes. 
+        private void Awake()
+        {
+            if (hasSpawned) return;
+            SpawnPersistentObjects();
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
+
---
+++ RPG/Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        public enum DestinationIdentifier
+        {
+            A,B,C
+        }
+        public String sceneToLoad = "Outside";
+        public Transform spawnPoint;
+        public DestinationIdentifier destination;
+        public float sceneFadeTime = 0.5f;
+        public float sceneWaitTime = 0.5f;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(this.gameObject);
+            
+            Fader fader = FindObjectOfType<Fader>();
+            
+            yield return fader.FadeOut(sceneFadeTime); //fades out the scene
+            yield return SceneManager.LoadSceneAsync(sceneToLoad); //asynchronously loads the scene
+            
+            Portal newPortal = GetOtherPortal(); //find the portal in the new scene
+            UpdatePlayerLocation(newPortal); //update the player's position and rotation to the new spawn point
+            
+            yield return new WaitForSeconds(sceneWaitTime); //Wait a few seconds so that the camera can stabilise
+            yield return fader.FadeIn(sceneFadeTime); //fades in the scene 
+
+            Destroy(this.gameObject);
+        }
+
+        private void UpdatePlayerLocation(Portal newPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = newPortal.spawnPoint.position;
+            player.transform.rotation = newPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != this.destination) continue;
+                return portal;
+            }
+
+            return null;
+        }
+    }
+} 
