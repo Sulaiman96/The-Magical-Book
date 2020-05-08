3efe39ba7bdc019b99c79cd474976e83c7f83001
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2020-05-08 17:33:30 +0100

    Enemy Health bar and damage indication is now present

---
+++ RPG/Assets/Scripts/Health/HealthDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Health
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        private Health _health;
+        private Text _text;
+
+        private void Start()
+        {
+            _text = GetComponent<Text>();
+        }
+
+        private void Awake()
+        {
+            _health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        }
+
+        private void Update()
+        {
+            _text.text = String.Format("{0:0}%" ,_health.GetPercentageHealth());
+        }
+    }
+}
+
