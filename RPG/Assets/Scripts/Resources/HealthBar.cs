1bdad85b81d114625aa9e1a7b126c9bd1a2d5b19
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2020-05-20 09:22:26 +0100

    Added basic audio

---
+++ RPG/Assets/Scripts/Resources/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+namespace RPG.Resources
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Health healthComponent = null;
+        [SerializeField] private RectTransform foreground = null;
+        [SerializeField] private Canvas rootCanvas = null;
+        
+        private void Update()
+        {
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0)
+            || Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
+            
+            foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
+        }
+    }
+}
